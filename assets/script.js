6cca5d16ab26c4821391f1c6687d3ae6d4197039
tonytran97 <tonytran_97@live.com>
2022-08-02 16:20:29 -0700

    applied floppy icon, applied provided css styles

--- assets/script.js
+++ assets/script.js
@@ -2,16 +2,18 @@
 var currentDay = moment().format("dddd, MMMM Do YYYY h:mma");
 $("#currentDay").text(currentDay);
 
+var currentHour = moment().format("h");
+
 // hours in a standard business job (9AM-5PM)
 var workHours = 9;
 
 // for loop to generate the time blocks in this day
 for (i = 0; i < workHours; i++) {
     var columns = $("<div>");
-    columns.addClass("row align-items-end");
+    columns.addClass("row time-block");
 
     var timeColumns = $("<div>");
-    timeColumns.addClass("col-2");
+    timeColumns.addClass("col-2 hour");
     if (i <= 2) {
         timeColumns.text(i + workHours + ":00AM");
     } else if (i == 3) {
@@ -19,14 +21,20 @@ for (i = 0; i < workHours; i++) {
     } else
         timeColumns.text(i + workHours - 12 + ":00PM");
 
-    var eventColumns = $("<div>");
-    eventColumns.addClass("col-8");
+    var eventColumns = $("<textarea>");
+    // insert function for color coordinated time blocks
+    eventColumns.addClass("col-8 description future");
 
     var saveBtn = $("<button>");
-    saveBtn.addClass("col-2");
+    saveBtn.addClass("col-2 saveBtn");
+
+    var saveIcon = $("<i>");
+    saveIcon.addClass("fas fa-save");
 
     $(".container").append(columns);
     columns.append(timeColumns);
     columns.append(eventColumns);
+    saveBtn.append(saveIcon);
     columns.append(saveBtn);
 }
+
