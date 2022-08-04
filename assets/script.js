072f4187da6dd0a98fe0dfcd74068182b012fe0f
tonytran97 <tonytran_97@live.com>
2022-08-04 11:17:49 -0700

    color coded time blocks

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@
 var currentDay = moment().format("dddd, MMMM Do YYYY h:mma");
 $("#currentDay").text(currentDay);
 
-var currentHour = moment().format("h");
+var currentHour = moment().format("H");
 
 // hours in a standard business job (9AM-5PM)
 var workHours = 9;
@@ -12,18 +12,20 @@ for (i = 0; i < workHours; i++) {
     var columns = $("<div>");
     columns.addClass("row time-block");
 
+    var eventColumns = $("<textarea>");
+    eventColumns.addClass("col-8 description");
+
     var timeColumns = $("<div>");
     timeColumns.addClass("col-2 hour");
     if (i <= 2) {
         timeColumns.text(i + workHours + ":00AM");
+        color();
     } else if (i == 3) {
         timeColumns.text(i + workHours + ":00PM");
+        color();
     } else
         timeColumns.text(i + workHours - 12 + ":00PM");
-
-    var eventColumns = $("<textarea>");
-    // insert function for color coordinated time blocks
-    eventColumns.addClass("col-8 description future");
+    color();
 
     var saveBtn = $("<button>");
     saveBtn.addClass("col-2 saveBtn");
@@ -38,3 +40,12 @@ for (i = 0; i < workHours; i++) {
     columns.append(saveBtn);
 }
 
+// function to color-code the time blocks
+function color() {
+    if ((i + workHours) == currentHour) {
+        eventColumns.addClass("present");
+    } else if ((i + workHours) < currentHour) {
+        eventColumns.addClass("past");
+    } else
+        eventColumns.addClass("future");
+}
